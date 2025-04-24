649a235fd641a507b6970d4d485e76baf58a79a6
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-04-24 11:26:53 +0300

    add user interface without funkcional

--- CourseWork/MyForm1.h
+++ CourseWork/MyForm1.h
@@ -35,6 +35,21 @@ namespace CourseWork {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ button10;
+	private: System::Windows::Forms::Button^ button11;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Button^ button12;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	protected:
 
 	private:
@@ -51,32 +66,203 @@ namespace CourseWork {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button10 = (gcnew System::Windows::Forms::Button());
+			this->button11 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button12 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(97, 104);
+			this->button1->Location = System::Drawing::Point(1092, 12);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(78, 42);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Назад";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 400);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(180, 40);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Моя сборка";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(1008, 12);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(78, 42);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Выход";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 17, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(209, 14);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(579, 40);
+			this->textBox1->TabIndex = 3;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(794, 14);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(73, 40);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"Поиск";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(12, 78);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(180, 40);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"Материнсие платы";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(12, 124);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(180, 40);
+			this->button6->TabIndex = 6;
+			this->button6->Text = L"Видеокарты";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(12, 170);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(180, 40);
+			this->button7->TabIndex = 7;
+			this->button7->Text = L"Процессоры";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(12, 216);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(180, 40);
+			this->button8->TabIndex = 8;
+			this->button8->Text = L"Системы охлаждения";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(12, 262);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(180, 40);
+			this->button9->TabIndex = 9;
+			this->button9->Text = L"Напокители";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this->button10->Location = System::Drawing::Point(12, 308);
+			this->button10->Name = L"button10";
+			this->button10->Size = System::Drawing::Size(180, 40);
+			this->button10->TabIndex = 10;
+			this->button10->Text = L"Оперативная память";
+			this->button10->UseVisualStyleBackColor = true;
+			// 
+			// button11
+			// 
+			this->button11->Location = System::Drawing::Point(12, 354);
+			this->button11->Name = L"button11";
+			this->button11->Size = System::Drawing::Size(180, 40);
+			this->button11->TabIndex = 11;
+			this->button11->Text = L"Блоки питания";
+			this->button11->UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->button12);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(209, 78);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(200, 275);
+			this->panel1->TabIndex = 12;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(3, 3);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(194, 194);
+			this->pictureBox1->TabIndex = 13;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(69, 200);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(51, 16);
+			this->label1->TabIndex = 13;
+			this->label1->Text = L"test text";
+			// 
+			// button12
+			// 
+			this->button12->Location = System::Drawing::Point(57, 219);
+			this->button12->Name = L"button12";
+			this->button12->Size = System::Drawing::Size(90, 30);
+			this->button12->TabIndex = 14;
+			this->button12->Text = L"Подробнее";
+			this->button12->UseVisualStyleBackColor = true;
 			// 
 			// MyForm1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->ClientSize = System::Drawing::Size(1182, 653);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->button11);
+			this->Controls->Add(this->button10);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->MaximumSize = System::Drawing::Size(1200, 700);
+			this->MinimumSize = System::Drawing::Size(1200, 700);
 			this->Name = L"MyForm1";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MyForm1";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		this->Close();
 	}
-	};
+};
 }
